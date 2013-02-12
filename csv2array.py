ba16ccb01a3212f9704362a87f00575fbfd00f1d
Stefan Schmitt <sschmitt@gmx.de>
2013-02-12 22:56:56 +0100

    add option for formatting as preprocessor macro

--- csv2array.py
+++ csv2array.py
@@ -8,8 +8,17 @@ parser.add_argument('ifile', type=argparse.FileType('r'), default='-')
 parser.add_argument('-w', dest='width', type=int, default=0)
 parser.add_argument('-o', dest='ofile', type=argparse.FileType('w'), default='-')
 parser.add_argument('-d', dest='delimiter', default=',')
+parser.add_argument('-m', dest='macro', action='store_true')
+
 args = parser.parse_args()
 
+if args.macro:
+    eol_line = ' \\\n'
+    eol_final = '\n'
+else:
+    eol_line = '\n'
+    eol_final = ';\n'
+    
 ifilereader = csv.reader(args.ifile, delimiter=args.delimiter)
 data = []
 for row in ifilereader:
@@ -27,13 +36,13 @@ def format_row(row):
     return str.join(', ', formatted_elements)
 
 if type(data[0]) is list:       # 2D array
-    args.ofile.write('{\n')
+    args.ofile.write('{' + eol_line)
     for row in data[:-1]:
-        args.ofile.write('\t{ ' + format_row(row) + ' },\n')
-    args.ofile.write('\t{ ' + format_row(data[-1]) + ' }\n')
-    args.ofile.write('};\n')
+        args.ofile.write('\t{ ' + format_row(row) + ' },' + eol_line)
+    args.ofile.write('\t{ ' + format_row(data[-1]) + ' }' + eol_line)
+    args.ofile.write('}' + eol_final)
 else:                           # 1D array
-    args.ofile.write('{ ' + format_row(data) + ' };\n')
+    args.ofile.write('{ ' + format_row(data) + ' }' + eol_final)
 
 args.ifile.close()
 args.ofile.close()
